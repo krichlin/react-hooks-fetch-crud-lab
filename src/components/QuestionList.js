6dfdf31c159e2c4c8fab204d50dfb19bdefa134c
Ken Richlin <krichlin@gmail.com>
2024-04-30 11:16:19 -0400

    first deliverable complete

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+
+  // set state variable for setQuestions
+  const [questions, setQuestions] = useState([]);
+
+  // fetch components here
+  useEffect(() =>  {
+    fetch("http://localhost:4000/questions")
+    .then((r) => r.json())
+    .then((questions) => {
+      setQuestions(questions);
+    });
+  }, [])
+
+  const questionItems = questions.map((q) => (
+    <QuestionItem
+      key={q.id}
+      question={q}
+    />
+  ))
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
